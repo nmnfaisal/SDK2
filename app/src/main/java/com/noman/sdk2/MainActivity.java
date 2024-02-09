f43fa5c6349d76e964fa4085cd7b1dbe1a1d07c1
Noman Faisal <nmnfaisal@gmail.com>
2024-02-09 16:04:56 +0500

    Initial commit

---
+++ app/src/main/java/com/noman/sdk2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.noman.sdk2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.noman.toaster.MyToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyToast.showToast(this, "this Toast is in SDK");
+
+
+    }
+}
---
+++ toaster/src/main/java/com/noman/toaster/MyToast.java
@@ -0,0 +1,13 @@
+package com.noman.toaster;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyToast {
+
+    public static void showToast(Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+
+    }
+}
